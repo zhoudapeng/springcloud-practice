7ed56fbe89694b089f1551708dc81f5354c0d129
zhoudapeng <zhoudapeng@ipaynow.cn>
2018-06-07 16:02:17 +0800

    edit config-client-dev.properties

---
+++ config-client/src/main/java/com/zdp/springcloud/config/client/ConfigClientApplication.java
@@ -0,0 +1,23 @@
+package com.zdp.springcloud.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${name}")
+    String name;
+    @RequestMapping(value = "/my-name")
+    public String hi(){
+        return "your name is " + name;
+    }
+}
